7c43ffde01a35e6a14e7832fc2848110779a76a0
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-11-29 17:10:18 -0400

    login form

--- sgi/home/urls.py
+++ sgi/home/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView
+from .forms import SGI_AuthenticationForm
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    
+    path('auth/login/', LoginView.as_view(authentication_form=SGI_AuthenticationForm), name='login'),
+
+    path('auth/', include('django.contrib.auth.urls')),
 ]
