72748a5bd7e7d5efeddabe75fc6944295a52191e
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-11-29 11:53:12 -0400

    test reverse home url

--- sgi/home/tests.py
+++ sgi/home/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
 
 class HomeViewsTest(TestCase):
     
     def test_home_root_view(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
--- sgi/home/urls.py
+++ sgi/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/default.html')),
+    path('', TemplateView.as_view(template_name='home/default.html'), name='home'),
 ]
