16ca8e0d63ff92a1c0181b30f408581d34ad1fab
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-12-16 13:00:11 -0300

    nome e sigla mixin

--- sgi/commons/models.py
+++ sgi/commons/models.py
@@ -9,3 +9,10 @@ class AuditableModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class NomeESiglaMixin(object):
+    
+    sigla = models.CharField(max_length=20)
+
+    nome = models.CharField(max_length=140)    
